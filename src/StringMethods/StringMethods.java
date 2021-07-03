9eca4fc0b709687a29cf36368aad8708b9b4f1ec
meijiahe <meijiahe@meijias-mbp.lan>
2021-07-03 12:53:03 -0700

    String Methods half finished

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,13 +32,21 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if (s1.length()>s2.length()) {
+			return s1;
+		}
+		return s2;
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+		for (int i =0; i<s.length();i++) {
+			if (s.contains("underscores")) {
+				s = s.replace(" ", "_");
+			}
+		}
+		return s;
 	}
 
 	
@@ -46,6 +54,7 @@ public class StringMethods {
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
+		
 		return null;
 	}
 	
